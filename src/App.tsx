f50480e6e78ed68a88fe298cf5408cd74cb9de2d
hojncode <glicm12@gmail.com>
2022-10-30 21:25:34 +0900

    [study] ES6 문법- const {currentTarget:{value,id,tagName,width}}=event; 사용시 여러개의 속성값을 보다 간편하게 가져올 수 있다.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,38 @@
+import React, { useState } from "react";
 import Circle from "./Circle";
 
+
+
 function App() {
-  return (
+  
+    const [value, setValue] = useState("");
+
+    const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+      const {
+        currentTarget : {value},
+      } = event;
+      setValue(value);
+    };
+
+    const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+      event.preventDefault();
+      console.log("hello", value);
+    };
+
+    return (
     <div>
       <Circle borderColor="yellow" bgColor="teal"/>
       <Circle text="props from parent" bgColor="tomato"/>
-      </div>
+
+      <form onSubmit={onSubmit}>
+        <input
+          value={value}
+          onChange={onChange}
+          type="text"
+          placeholder="username"
+        />
+      </form>
+    </div>
   )
 }
 
