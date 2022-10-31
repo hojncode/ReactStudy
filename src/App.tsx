bdf362034f667bd8f5e996dc6b2e924df90c7c62
hojncode <glicm12@gmail.com>
2022-10-31 13:27:00 +0900

    [study] TypeScript 에서 styled-components 사용하기 - DefaultTheme는 기본적으로 props.theme의 인터페이스로 사용됨. - 인터페이스는 비어 있으므로 확장해야함. 1. styled.d.ts 를 만든다. (참고: d.ts 는 declaration file 이라는 뜻이다.) 2. theme.ts (테마) 를 만든다. 3. index.tsx 에 2에서 만든 테마를 주입한다. 4. app.tsx 에서 props 로 받아 사용한다.

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,59 @@
 import React, { useState } from "react";
 import Circle from "./Circle";
+import styled from "styled-components";
 
 
 
-function App() {
+// function App() {
   
-    const [value, setValue] = useState("");
+//     const [value, setValue] = useState("");
 
-    const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-      const {
-        currentTarget : {value},
-      } = event;
-      setValue(value);
-    };
+//     const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+//       const {
+//         currentTarget : {value},
+//       } = event;
+//       setValue(value);
+//     };
 
-    const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-      event.preventDefault();
-      console.log("hello", value);
-    };
+//     const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+//       event.preventDefault();
+//       console.log("hello", value);
+//     };
 
+//     return (
+//     <div>
+//       <Circle borderColor="yellow" bgColor="teal"/>
+//       <Circle text="props from parent" bgColor="tomato"/>
+
+//       <form onSubmit={onSubmit}>
+//         <input
+//           value={value}
+//           onChange={onChange}
+//           type="text"
+//           placeholder="username"
+//         />
+//       </form>
+//     </div>
+//   )
+
+// }
+
+
+  const Container = styled.div`
+    background-color: ${(props) => props.theme.bgColor};
+  `
+
+  const H1 =styled.div`
+    color: ${(props)=> props.theme.textColor};
+  `
+
+  function App() {
     return (
-    <div>
-      <Circle borderColor="yellow" bgColor="teal"/>
-      <Circle text="props from parent" bgColor="tomato"/>
+      <Container>
+        <H1>hahahahahahah</H1>
+      </Container>
+    )
+  }
 
-      <form onSubmit={onSubmit}>
-        <input
-          value={value}
-          onChange={onChange}
-          type="text"
-          placeholder="username"
-        />
-      </form>
-    </div>
-  )
-}
 
 export default App;
